a524a2a5eac12d9f59a7667bb09c3f232e1e0fb5
kushalbhana <kushalbhana2050@gmail.com>
2025-01-25 23:53:38 +0530

    working checkpoint

--- apps/web/components/shared/Settings.tsx
+++
@@ -1,8 +0,0 @@
-import { IoIosSettings } from "react-icons/io";
-
-
-export default function Spactate(){
-    return(
-        <div className="flex items-center min-h-16"><IoIosSettings className="mr-2 mt-1"/>Settings</div>
-    )
-}
